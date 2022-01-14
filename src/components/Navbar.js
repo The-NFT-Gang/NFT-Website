410d3ed8ca76a28e075b4dc28a6d2d3005a53fa5
Majid Joseph <majid.joseph7@gmail.com>
2022-01-13 19:26:15 -0500

    demo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -58,7 +58,7 @@ function Navbar() {
                 className='nav-links'
                 onClick={closeMobileMenu}
               >
-                About
+                About- test
               </Link>
             </li>
 
