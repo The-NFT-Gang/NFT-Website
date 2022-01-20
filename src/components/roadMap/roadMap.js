0fbb4ac36243d3aaf123f6b14d8d0daf250b7204
CryptAlchemy <CryptAlchemy@users.noreply.github.com>
2022-01-20 00:40:00 -0500

    Small roadmap font changes

--- src/components/roadMap/roadMap.js
+++ src/components/roadMap/roadMap.js
@@ -7,10 +7,10 @@ import { Image } from 'react-bootstrap';
 const RoadMap = ({text, title, description}) => {
     return (
         <div href={text} style={{ "color": "unset" }}>
-            <div className="roadmap-title fs-bold" style={{ "opacity": "1", "transform": "none" }}>
+            <div className="roadmap-title fs-bold fs-3" style={{ "opacity": "1", "transform": "none" }}>
                 {title}
             </div>
-            <div className="roadmap-description" style={{"opacity": "1", "transform": "none" }}>
+            <div className="text text-white">
                 {description}
             </div>
         </div>
