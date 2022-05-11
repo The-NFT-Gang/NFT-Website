75ef9deeaea9878b7e338e883f31b4e86bb1b05c
Hunter Denomme <hunterdenomme55@gmail.com>
2022-05-11 18:18:09 -0400

    Added Home page

---
+++ src/components/InfoSection/data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Project ',
+    headline: 'The worlds first NFT lottery',
+    description:
+      'By owning one of these sloths your address will be placed into a pool to win ETH.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/temp_nft_1.png'),
+    alt: 'Majid',
+    dark: true,
+    primary: true,
+    darkText: false
+  };
